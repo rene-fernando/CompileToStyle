30850f8f961f15dc8724cb60c6e4514b234eda92
Katherine Raguini <110441766+kcraguini@users.noreply.github.com>
2024-10-26 20:09:42 -0500

    add video element

---
+++ script.js
@@ -0,0 +1,9 @@
+const video = document.getElementById('video');
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        video.srcObject = stream;
+    })
+    .catch(error => {
+        console.error('Error accessing media devices.', error);
+    });
